5652b6b2cac79fd1c64ecbf1938dfe429c4f201e
AaronHsieh <piafyoyo06@gmail.com>
2024-08-15 01:22:09 +0800

    引入 style.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// 引入 Bootstrap 相關檔案
 import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { createApp } from "vue";
+import './style.css'
 import App from "./App.vue";
 
 createApp(App).mount("#app");
