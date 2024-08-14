159ccdeab7b0689af0248945269ace24a6ec2737
AaronHsieh <piafyoyo06@gmail.com>
2024-08-14 23:10:47 +0800

    引入並使用品項列表元件，透過 props 來帶入品項資料

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, computed } from "vue";
 
+// 引入品項列表元件
+import itemLists from "./components/itemLists.vue";
+
 // 引入資料檔案
 import { data } from "./data/items.js";
 
@@ -56,20 +59,7 @@ const itemSubtotal = (item) => {
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-4">
-        <div class="list-group">
-          <a
-            v-for="drink in drinks"
-            :key="drink.id"
-            href="#"
-            class="list-group-item list-group-item-action"
-            @click.prevent="addToCart(drink)">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">{{ drink.name }}</h5>
-              <small>${{ drink.price }}</small>
-            </div>
-            <p class="mb-1">{{ drink.description }}</p>
-          </a>
-        </div>
+        <itemLists :drinks="drinks" :addToCart="addToCart" />
       </div>
       <div class="col-md-8">
         <table class="table">
