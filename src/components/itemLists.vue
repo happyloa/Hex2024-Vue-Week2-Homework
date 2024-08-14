8ff3c11df1e2f42fb02400cd85aaff4f9a351a07
AaronHsieh <piafyoyo06@gmail.com>
2024-08-14 23:10:16 +0800

    拆分出品項列表元件

---
+++ src/components/itemLists.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  drinks: Array,
+  addToCart: Function,
+});
+</script>
+
+<template>
+  <aside class="list-group">
+    <a
+      v-for="drink in drinks"
+      :key="drink.id"
+      href="#"
+      class="list-group-item list-group-item-action"
+      @click.prevent="addToCart(drink)">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">{{ drink.name }}</h5>
+        <small>${{ drink.price }}</small>
+      </div>
+      <p class="mb-1">{{ drink.description }}</p>
+    </a>
+  </aside>
+</template>
