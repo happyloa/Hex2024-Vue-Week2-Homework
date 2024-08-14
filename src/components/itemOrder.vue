ff5714ad59dce3269c0bbbc61be685a52406b204
AaronHsieh <piafyoyo06@gmail.com>
2024-08-14 23:47:14 +0800

    移除多餘的樣式並將欄位寬度改為全寬

--- src/components/itemOrder.vue
+++ src/components/itemOrder.vue
@@ -8,8 +8,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="row justify-content-center">
-    <div class="col-8">
+  <div class="row">
+    <div class="col-12">
       <div
         v-if="!order.id"
         class="alert alert-secondary text-center"
